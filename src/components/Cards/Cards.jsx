ac3b3ef7f4c4a4254551147c2b3244c4529ff3b8
Miguel Chan <miguelchan2002@outlook.es>
2023-09-04 09:47:46 -0600

    first commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -7,7 +7,7 @@ const Cards = () => {
                 <div className="h-3/4 bg-card bg-no-repeat bg-cover rounded-lg"></div>
                 <div className="h-1/4 flex flex-col">
                     <Heading2 text='J. Cole - Atm' />
-                    <span className="text-xs uppercase font-markProMedium">9 february, 2019 | Spektre</span>
+                    <span className="text-xs uppercase font-markProMedium">7 february, 2019 | Spektre</span>
                 </div>
             </div>
             <div className="h-72 flex flex-col gap-4 overflow-hidden">
