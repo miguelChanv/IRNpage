2bc7fe2e10cc7c6be1c0dc627e8a861d79863b5f
Miguel Chan <miguelchan2002@outlook.es>
2023-09-04 22:15:10 -0600

    first commit

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -4,14 +4,14 @@ const Navbar = () => {
   return (
     <header className='w-full bg-transparent text-white absolute top-0'>
       <nav className='max-w-7xl mx-auto flex justify-between p-5'>
-        <h1>IRN</h1>
+        <h1>IRN COMUNICACIONES</h1>
         <ul className='flex gap-4'>
-          <li><Links path="/" text="Music" /></li>
+          <li><Links path="/" text="Musica" /></li>
           <li><Links path="/" text="Eventos" /></li>
-          <li><Links path="/" text="Artistas" /></li>
+          <li><Links path="/" text="Invitados" /></li>
           <li><Links path="/" text="Blog" /></li>
           <li><Links path="/" text="Album" /></li>
-          <li><Links path="/" text="Contact" /></li>
+          <li><Links path="/" text="Contactanos" /></li>
         </ul>
         <div className="flex gap-2 items-center">
           <span className="">EN</span>
