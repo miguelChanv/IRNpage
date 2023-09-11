e745bf6cb9edd1d2bab6c1f335b2f6799ef812ec
Roberto12541 <robertomiranda1254@gmail.com>
2023-09-11 15:36:14 -0600

    Chan pendejo

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -24,6 +24,27 @@ const Cards = () => {
                     <span className="text-xs uppercase font-markProMedium">9 february, 2019 | Spektre</span>
                 </div>
             </div>
+            <div className="h-72 flex flex-col gap-4 overflow-hidden">
+                <div className="h-3/4 bg-card bg-no-repeat bg-cover rounded-lg"></div>
+                <div className="h-1/4 flex flex-col">
+                    <Heading2 text='J. Cole - Atm' />
+                    <span className="text-xs uppercase font-markProMedium">7 february, 2019 | Spektre</span>
+                </div>
+            </div>
+            <div className="h-72 flex flex-col gap-4 overflow-hidden">
+                <div className="h-3/4 bg-card bg-no-repeat bg-cover rounded-lg"></div>
+                <div className="h-1/4 flex flex-col">
+                    <Heading2 text='J. Cole - Atm' />
+                    <span className="text-xs uppercase font-markProMedium">9 february, 2019 | Spektre</span>
+                </div>
+            </div>
+            <div className="h-72 flex flex-col gap-4 overflow-hidden">
+                <div className="h-3/4 bg-card bg-no-repeat bg-cover rounded-lg"></div>
+                <div className="h-1/4 flex flex-col">
+                    <Heading2 text='J. Cole - Atm' />
+                    <span className="text-xs uppercase font-markProMedium">9 february, 2019 | Spektre</span>
+                </div>
+            </div>
         </div>
     )
 }
